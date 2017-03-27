75669c45e32fac6aed2b15ac1d98f703b2e9ed39
Septian Wibisono <asep.gelo@gmail.com>
2017-03-27 12:22:48 +0700

    add xenial security branch

---
+++ xenial-sec.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# based on https://github.com/textlab/glossa/blob/master/script/build_ubuntu_essential.sh
+
+TAG=quay.io/septian/ubuntu-essential
+VERSION=16.04-sec
+CODENAME=xenial
+REVISION=20170322
+
+set -ve
+
+# Build the official image from https://github.com/tianon/docker-brew-ubuntu-core
+wget -q https://raw.githubusercontent.com/tianon/docker-brew-ubuntu-core/dist/${CODENAME}/Dockerfile
+wget -q https://partner-images.canonical.com/core/${CODENAME}/${REVISION}/ubuntu-${CODENAME}-core-cloudimg-amd64-root.tar.gz
+
+docker build -t ubuntu:${CODENAME}-${REVISION} .
+
+rm -f Dockerfile ubuntu-${CODENAME}-core-cloudimg-amd64-root.tar.gz
+
+# Based on https://github.com/textlab/glossa/blob/master/script/build_ubuntu_essential.sh
+docker build -t ubuntu-essential-multilayer - <<EOF
+FROM ubuntu:${CODENAME}-${REVISION}
+# Make an exception for apt: it gets deselected, even though it probably shouldn't.
+RUN apt-get update && apt-get upgrade -y
+RUN export DEBIAN_FRONTEND=noninteractive
+RUN dpkg --clear-selections && echo "apt install" | dpkg --set-selections
+RUN apt-get --purge -y --allow-remove-essential dselect-upgrade
+RUN apt-get purge -y --allow-remove-essential init systemd
+RUN apt-get purge -y --allow-remove-essential libapparmor1 libcap2 libcryptsetup4 libdevmapper1.02.1 libkmod2 libseccomp2
+RUN apt-get --purge -y autoremove
+RUN dpkg-query -Wf '\${db:Status-Abbrev}\t\${binary:Package}\n' | \
+      grep '^.i' | cut -f2 | sed 's/.*/\0 install/' | dpkg --set-selections
+RUN rm -rf /var/cache/apt /var/lib/apt/lists /var/cache/debconf/* /var/log/*
+EOF
+
+TMP_FILE="$(mktemp -t ubuntu-essential-XXXXXX).tar.gz"
+
+docker run --rm -i ubuntu-essential-multilayer tar zpc --exclude=/etc/hostname \
+  --exclude=/etc/resolv.conf --exclude=/etc/hosts --one-file-system / > "$TMP_FILE"
+docker rmi ubuntu-essential-multilayer
+
+docker import - ubuntu-essential-nocmd < "$TMP_FILE"
+
+docker build -t ${TAG}:${VERSION} - <<EOF
+FROM ubuntu-essential-nocmd
+CMD ["/bin/bash"]
+EOF
+
+docker rmi ubuntu-essential-nocmd
+rm -f "$TMP_FILE"
+
+# Set tags to release
+docker tag ${TAG}:${VERSION} ${TAG}:${VERSION}-${REVISION}
+docker tag ${TAG}:${VERSION} ${TAG}:latest
